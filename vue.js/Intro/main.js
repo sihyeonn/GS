d1c43150954dbdbed7bc82e69c8d0cac62f7e0e5
Sihyeon Jang <uneedsihyeon@gmail.com>
2020-01-26 03:31:08 -0500

    2. Attribute Binding

--- vue.js/Intro/main.js
+++ vue.js/Intro/main.js
@@ -2,6 +2,7 @@ var app = new Vue({
   el: '#app',
   data: {
     product: 'Socks',
-    description: "A pair of warm, fuzzy socks."
+    image: "./assets/vmSocks-green.png",
+    link: "https://www.google.com"
   }
 })
