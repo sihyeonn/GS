b1010226ec3dfb84e312d0aef75d43af9102ad81
Sihyeon Jang <uneedsihyeon@gmail.com>
2020-01-26 04:03:12 -0500

    5. Event Handling

--- vue.js/Intro/main.js
+++ vue.js/Intro/main.js
@@ -5,6 +5,28 @@ var app = new Vue({
     image: "./assets/vmSocks-green.png",
     inStock: true,
     details: ["80% cotton", "20% polyester", "Gender-neutral"],
-    sizes: ['XS', 'S', 'M', 'L', 'XL', 'XXL']
+    variants: [
+      {
+        id: 2234,
+        color: "green",
+        image: "./assets/vmSocks-green.png"
+      }, {
+        id: 2235,
+        color: "blue",
+        image: "./assets/vmSocks-blue.png"
+      }
+    ],
+    cart: 0
+  },
+  methods: {
+    addToCart: function() {
+      this.cart += 1;
+    },
+    removeFromCart() {
+      if (0 < this.cart) this.cart -= 1;
+    },
+    updateProduct(image) {
+      this.image = image;
+    }
   }
 })
